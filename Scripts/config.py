2e399b15fd1465949db2d0f5980667c8ce110618
nomad <nomad@debian.nomaddebian.net>
2024-03-05 23:19:59 +0100

    Notebook: 01 - DataSourcing

---
+++ Scripts/config.py
@@ -0,0 +1,141 @@
+Cities = [
+            {
+                "CityID": "AO",
+                "CityName": "Aosta",
+                "Latitude": 45.735062, 
+                "Longitude": 7.313080
+            },
+    
+            {
+                "CityID": "TO",
+                "CityName": "Torino",
+                "Latitude": 45.070339,
+                "Longitude": 7.686864
+            },
+    
+            {
+                "CityID": "TN",
+                "CityName": "Trento",
+                "Latitude": 46.066669,
+                "Longitude": 11.129070
+            },
+    
+            {
+                "CityID": "MI",
+                "CityName": "Milano",
+                "Latitude": 45.464203, 
+                "Longitude": 9.189982
+            },
+
+            {
+                "CityID": "VE",
+                "CityName": "Venezia",
+                "Latitude": 45.440845, 
+                "Longitude": 12.315515
+            },
+            
+            {
+                "CityID": "TS",
+                "CityName": "Trieste",
+                "Latitude": 45.653599, 
+                "Longitude": 13.778520
+            },
+    
+            {
+                "CityID": "GE",
+                "CityName": "Genova",
+                "Latitude": 44.405651, 
+                "Longitude": 8.946256
+            },
+
+            {
+                "CityID": "FI",
+                "CityName": "Firenze",
+                "Latitude": 43.769562, 
+                "Longitude": 11.255814
+            },
+    
+            {
+                "CityID": "BO",
+                "CityName": "Bologna",
+                "Latitude": 44.494888, 
+                "Longitude": 11.342616
+            },
+            
+            {
+                "CityID": "AN",
+                "CityName": "Ancona",
+                "Latitude": 43.615849, 
+                "Longitude": 13.518740
+            },
+    
+            {
+                "CityID": "PG",
+                "CityName": "Perugia",
+                "Latitude": 43.110718, 
+                "Longitude": 12.390828
+            },
+    
+            {
+                "CityID": "RM",
+                "CityName": "Roma",
+                "Latitude": 41.902782, 
+                "Longitude": 12.496365
+            },
+    
+            {
+                "CityID": "NA",
+                "CityName": "Napoli",
+                "Latitude": 40.851799, 
+                "Longitude": 14.268120
+            },
+    
+            {
+                "CityID": "AQ",
+                "CityName": "L'Aquila",
+                "Latitude": 42.350700, 
+                "Longitude": 13.399930
+            },
+    
+            {
+                "CityID": "CB",
+                "CityName": "Campobasso",
+                "Latitude": 41.560090, 
+                "Longitude": 14.664800
+            },
+    
+            {
+                "CityID": "BA",
+                "CityName": "Bari",
+                "Latitude": 41.125912, 
+                "Longitude": 16.872110
+            },
+    
+            {
+                "CityID": "PZ",
+                "CityName": "Potenza",
+                "Latitude": 40.637241, 
+                "Longitude": 15.802220
+            },
+    
+            {
+                "CityID": "CZ",
+                "CityName": "Catanzaro",
+                "Latitude": 38.910542, 
+                "Longitude": 16.587761
+            },
+    
+            {
+                "CityID": "PA",
+                "CityName": "Palermo",
+                "Latitude": 38.115662, 
+                "Longitude": 13.361470
+            },
+    
+            {
+                "CityID": "CA",
+                "CityName": "Cagliari",
+                "Latitude": 39.215408, 
+                "Longitude": 9.109320
+            },
+]
